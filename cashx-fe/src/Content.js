bd784f403121e92d97aa729b16d87122622ddf2a
musangisilvia <musangisilvia@gmail.com>
2021-10-14 20:22:19 +0300

    add landing page: jsx + css

---
+++ cashx-fe/src/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Landing page content
+function Content() {
+    return (
+        <div className="content">
+            <h1>Learn how to trade risk free</h1>
+            <h2>Worry less about money and get to learn</h2>
+            <button>Get started for free</button>
+        </div>
+    )
+}
+
+export default Content
---
+++ cashx-fe/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Header component for the landing page
+function Header() {
+    return (
+        <header>
+            <h2>CashX</h2>
+            <button>Log In</button>
+        </header>
+    )
+}
+
+export default Header
