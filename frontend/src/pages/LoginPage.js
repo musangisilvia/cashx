f77e925127b22aebc5336ad92c72225b4b2dc0fc
musangisilvia <musangisilvia@gmail.com>
2021-10-16 22:38:06 +0300

    create login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,16 @@
 import "../styles/LoginPage.css";
+import FormHeader from "../components/FormHeader";
+import LoginForm from "../components/LoginForm";
+import FormFooter from "../components/FormFooter";
 
 const LoginPage = () => {
-  return <div className="common loginpage"></div>;
+  return (
+    <div className="common loginpage">
+      <FormHeader />
+      <LoginForm />
+      <FormFooter />
+    </div>
+  );
 };
 
 export default LoginPage;
