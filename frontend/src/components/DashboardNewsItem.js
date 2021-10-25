d076baba4c02c7b1de44dc1e8294d7622eeb9d68
Alfred Tuva <muindetuva@gmail.com>
2021-10-25 11:00:54 +0300

    Make the List component use an api call

--- frontend/src/components/DashboardNewsItem.js
+++ frontend/src/components/DashboardNewsItem.js
@@ -1,16 +1,16 @@
 import "../styles/DashboardNewsItem.css";
 
-const DashboardNewsItem = () => {
+const DashboardNewsItem = ({ blog }) => {
   return (
     <div className="dashboard-news-item">
       <img
         className="news-card-img-top"
-        src="https://images.unsplash.com/photo-1596526131158-52be64dcc208?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+        src={blog.image}
         alt="news-item-image"
       />
 
       <div className="news-card-body">
-        <p className="card-text">Facebook services down for over 6 hours</p>
+        <p className="card-text">{blog.headline}</p>
       </div>
     </div>
 
--- frontend/src/components/DashboardNewsList.js
+++ frontend/src/components/DashboardNewsList.js
@@ -1,14 +1,24 @@
+import useFetch from "../helpers/useFetch";
+
 import "../styles/DashboardNewsList.css";
 
 import DashboardNewsItem from "./DashboardNewsItem";
 
 const DashBoardNewsList = () => {
+
+  const {data: blogs, isPending, error} = useFetch('http://localhost:5000/api/news')
+
   return (
     <div className="dashboard-news-list">
       <h4>Trending News</h4>
-      <DashboardNewsItem />
-      <DashboardNewsItem />
-      <DashboardNewsItem />
+      {/* Conditional Rendering */}
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {
+        blogs && 
+          blogs.map((blog) => (<DashboardNewsItem blog={ blog }/>))
+      }
+        
     </div>
   );
 };
