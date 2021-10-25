d6ea36330bce119d5a4caf128bab84df5055f5b5
Alfred Tuva <muindetuva@gmail.com>
2021-10-25 14:14:48 +0300

    Make the carousel

--- frontend/src/components/StockCarouselItem.js
+++ frontend/src/components/StockCarouselItem.js
@@ -1,11 +1,28 @@
-import "../styles/StockCarouselItem.css"
+import "../styles/StockCarouselItem.css";
 
-function StockCarouselItem({num}){
+function StockCarouselItem({ stock }) {
   return (
     <div className="stock-carousel-item">
-      {num}
+      <div className="carousel-item-header">
+        <p>{stock.symbol}</p>
+        <p className="cash">{"$" + stock.latestPrice}</p>
+      </div>
+      <div className="carousel-item-trend">
+        {stock.change > 0 &&
+        <p className="up-stock">
+          <span><i class="uil uil-angle-up"></i></span>
+          {"+" + stock.changePercent + "%"} ({stock.change})
+        </p>
+        }
+        {stock.change <= 0 && 
+          <p className="down-stock">
+            <span><i class="uil uil-angle-down"></i></span>
+            {stock.changePercent + "%"} ({stock.change})
+          </p>
+        }
+      </div>
     </div>
-  )
+  );
 }
 
 export default StockCarouselItem;
