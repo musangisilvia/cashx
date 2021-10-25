337268ec553173e0c2d3731b7d20d7fcc6dc2fa9
Alfred Tuva <muindetuva@gmail.com>
2021-10-25 19:02:53 +0300

    Add Table component

---
+++ frontend/src/components/DashboardTable.js
@@ -0,0 +1,11 @@
+import "../styles/DashboardTable.css";
+
+const DashboardTable = () => {
+  return (
+    <div className="dashboard-table">
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default DashboardTable;
