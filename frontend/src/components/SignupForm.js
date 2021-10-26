453db777f106cebf3aa57a4a4e6166d599c75e9c
musangisilvia <musangisilvia@gmail.com>
2021-10-26 15:07:23 +0300

    handle submit of form

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,23 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/SignupForm.css'
 
 function SignupForm() {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {username, email, password};
+
+        fetch('http://localhost:5000/signup', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        }).then(() => {
+            console.log('new user added')
+        })
+
+    }
+
     return (
         <div>
-            <form className="signup">
+            <form className="signup" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label for="username">Username</label> <br/>
-                    <input type="text" name="username"/>
+                    <input
+                        type="text"
+                        name="username"
+                        required
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group"> 
                     <label for="email">Email Address</label> <br/>
-                    <input type="email" name="email"/>
+                    <input
+                        type="email" 
+                        name="email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label for="password">Password</label> <br/>
-                    <input type="password" name="password"/>
+                    <input 
+                        type="password" 
+                        name="password"
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
